6f9d222b3d2edf7cee58c2fbec43ca180779038d
Levi Muniz <levi.muniz17@gmail.com>
2021-05-20 19:54:21 -0600

    Update Checkstyle
    Resolves #28

--- src/main/java/com/snowypeaksystems/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/snowypeaksystems/exampleplugin/ExamplePlugin.java
@@ -6,7 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Levi Muniz on 7/29/20.
- * Copyright (c) Levi Muniz. All Rights Reserved.
+ *
+ * @author Copyright (c) Levi Muniz. All Rights Reserved.
  */
 public class ExamplePlugin extends JavaPlugin {
 
--- src/test/java/com/snowypeaksystems/exampleplugin/ExamplePluginTest.java
+++ src/test/java/com/snowypeaksystems/exampleplugin/ExamplePluginTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Tests for ExamplePlugin.
+ *
  * @author Copyright (c) Levi Muniz. All Rights Reserved.
  */
 public class ExamplePluginTest {
