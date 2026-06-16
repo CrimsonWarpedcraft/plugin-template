1ffe5840cca868a9ff793cc77990f17903a5ad62
Levi Muniz <levi.muniz17@gmail.com>
2026-06-16 01:22:34 -0600

    feat(deps): Migrate provided boilerplate to imported project

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/config/PluginConfig.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/config/PluginConfig.java
@@ -1,5 +1,6 @@
 package com.crimsonwarpedcraft.exampleplugin.config;
 
+import com.crimsonwarpedcraft.cwcommons.config.Config;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
