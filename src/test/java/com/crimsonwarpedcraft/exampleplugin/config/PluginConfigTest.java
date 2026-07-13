d7081003dcffae021d81385d906fa10ec75f5757
Levi Muniz <levi.muniz17@gmail.com>
2026-07-13 01:31:57 -0600

    chore(deps): upgrade cw-commons to v0.3.0 (#400)

--- src/test/java/com/crimsonwarpedcraft/exampleplugin/config/PluginConfigTest.java
+++ src/test/java/com/crimsonwarpedcraft/exampleplugin/config/PluginConfigTest.java
@@ -21,27 +21,27 @@ class PluginConfigTest {
   @Test
   void acceptsValidConfig() {
     PluginConfig config = new PluginConfig("Custom!", "Hello, {player}!");
-    assertDoesNotThrow(() -> new ConfigManager().validate(config));
+    assertDoesNotThrow(() -> ConfigManager.builder().build().validate(config));
   }
 
   @Test
   void rejectsBlankPongMessage() {
     PluginConfig config = new PluginConfig("", "Hello, {player}!");
     assertThrows(IllegalStateException.class,
-        () -> new ConfigManager().validate(config));
+        () -> ConfigManager.builder().build().validate(config));
   }
 
   @Test
   void rejectsBlankGreetMessage() {
     PluginConfig config = new PluginConfig("Pong!", "");
     assertThrows(IllegalStateException.class,
-        () -> new ConfigManager().validate(config));
+        () -> ConfigManager.builder().build().validate(config));
   }
 
   @Test
   void rejectsBlankCreepersKilledMessage() {
     PluginConfig config = new PluginConfig("Pong!", "Hello, {player}!", "");
     assertThrows(IllegalStateException.class,
-        () -> new ConfigManager().validate(config));
+        () -> ConfigManager.builder().build().validate(config));
   }
 }
