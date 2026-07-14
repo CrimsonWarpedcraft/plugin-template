23d5403cf48ac911c6f75e571a022db884cca50a
Levi Muniz <levi.muniz17@gmail.com>
2026-07-13 22:59:28 -0600

    fix(build): Upgrade run-paper to compatible version for v26

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     checkstyle
     id("com.github.spotbugs") version "6.5.8"
     id("com.gradleup.shadow") version "9.5.1"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
     java
 }
 
